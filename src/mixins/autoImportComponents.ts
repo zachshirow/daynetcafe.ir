6184f20088a3b6fe661d74883ae382ec36c99dc7
Zachary Shirmohammadli <zachshirow@gmail.com>
2025-02-02 16:59:03 +0330

    starting to work on link previews

--- src/mixins/autoImportComponents.ts
+++ src/mixins/autoImportComponents.ts
@@ -1,7 +1,9 @@
 import BlogCardLink from '../components/BlogCardLink.astro';
 import BlogCardLinkTags from '../components/BlogCardLinkTags.astro';
+import { LinkPreview } from 'astro-embed';
 
 export const components = {
     BlogCardLink, 
-    BlogCardLinkTags
+    BlogCardLinkTags, 
+    LinkPreview
 }

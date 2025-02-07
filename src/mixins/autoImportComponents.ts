62f2f8c823bc4f4b621ea3128e899675c3a2781e
Zachary Shirmohammadli <zachshirow@gmail.com>
2025-02-07 17:25:06 +0330

    added irankhodro blog cover

--- src/mixins/autoImportComponents.ts
+++ src/mixins/autoImportComponents.ts
@@ -1,9 +1,11 @@
 import BlogCardLink from '../components/BlogCardLink.astro';
 import Snippets from '@/components/Snippets.astro';
+import AstroCard from '@/components/AstroCard.astro';
 // import { LinkPreview } from 'astro-embed';
 // import BlogCardLinkTags from '../components/BlogCardLinkTags.astro';
 
 export const components = {
     BlogCardLink, 
-    Snippets
+    Snippets,
+    AstroCard
 }

9005d398b0520fa93b3beba877735c1aa551494d
Zachary Shirmohammadli <zachshirow@gmail.com>
2025-02-26 08:55:20 +0330

    fixed some issues with the pagefind UI.

--- src/components/ui/mode-toggle.tsx
+++ src/components/ui/mode-toggle.tsx
@@ -48,7 +48,7 @@ export function ModeToggle() {
         >
           <Sun className="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
+          <span className="sr-only">تغییر حالت نمایش</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end" className="bg-background">

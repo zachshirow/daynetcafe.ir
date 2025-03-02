6c36b8fe6374916857ca5f37d6e28e1abf0868ac
Zachary Shirmohammadli <zachshirow@gmail.com>
2025-03-02 08:16:19 +0330

    added some new content related to plate swap services

--- src/reviews.ts
+++ src/reviews.ts
@@ -5,14 +5,14 @@ export const reviews: REVIEW[] = [
         name: "جمشید سالمی",
         quote: "خیلی از روش کار شون خوشم اومد. فکر نکنم تو گنبد کافی نتی اینقد سریع و راحت باشه.",
         rating: 5,
-        image: "/reviews/jamshid.png",
+        image: "/reviews/jamshid.jpg",
         show: true,
     },
     {
         name: "بنیامین مارامایی",
         quote: "کافی نت دی، کافی نت آپدیت و به روزی هست که در سریعترین زمان بهترین کیفیت خدمات رو بهتون ارائه میده.",
         rating: 5,
-        image: "/reviews/benyamin.png",
+        image: "/reviews/benyamin.jpg",
         show: true,
     },
     // {
